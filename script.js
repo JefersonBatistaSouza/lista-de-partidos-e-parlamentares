0331a6a0e0ebe2894c0a68fd490a27606a4f2110
JefersonBatistaSouza <jefersonb@live.com>
2022-05-21 18:35:58 -0400

    Melhoria 01

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function getPartidos(link) {
             for (var i = 0; i < data['dados'].length; i++) {
                 html += '\
                 <div class="input-check-partidos">\
-                    <input type="checkbox" id="partido'+ data['dados'][i]['id'] + '" name="partidos">\
-                    <label for="partidos">'+ data['dados'][i]['nome'] + ' (' + data['dados'][i]['sigla'] + ')</label>\
+                    <label for="partidos"> <input type="checkbox" id="partido'+ data['dados'][i]['id'] + '" name="partidos">\
+                    '+ data['dados'][i]['nome'] + ' (' + data['dados'][i]['sigla'] + ')</label>\
                     <button type="button" id="'+ data['dados'][i]['id'] + '" onclick="getParlamentar(this.id)">ver Parlamentares</button>\
                 </div>\
                 <div id="lista-de-parlamentares'+ data['dados'][i]['id'] + '"></div>';
@@ -26,7 +26,6 @@ function getPartidos(link) {
 
 function getParlamentar(id) {
     if ($("#partido" + id).is(':checked')) {
-        console.log(id);
         $.ajax({
             type: 'GET',
             url: 'https://dadosabertos.camara.leg.br/api/v2/partidos/'+id+'/membros/',
@@ -34,11 +33,11 @@ function getParlamentar(id) {
                 $("#lista-de-parlamentares").html("carregando...");
             },
             success: function (data) {
-                let html = "<strong>Parlamentares:</strong><ul>"
+                let html = '<pre id="pre'+id+'"><strong>Parlamentares:</strong><ul>';
                 for (var i = 0; i < data['dados'].length; i++) {
-                    html += '<li id="palamentar'+ data['dados'][i]['id'] + '">'+ data['dados'][i]['nome'] + '</li>';
+                    html += '<li id="parlamentar'+ data['dados'][i]['id'] + '">'+ data['dados'][i]['nome'] + '</li>';
                 }
-                html+='</ul>';
+                html+='</ul><button id="'+id+'"onclick="esconderParlamentares(this.id)">Esconder</button></pre>';
                 $("#lista-de-parlamentares"+id).html(html);
             },
             error: function (data) {
@@ -49,4 +48,12 @@ function getParlamentar(id) {
         alert("SELECIONE O PARTIDO");
     }
 }
+
+
+function esconderParlamentares(id){
+    $("#partido" + id).prop("checked", false);
+    $("#pre"+id).hide();
+}
+
+
 
